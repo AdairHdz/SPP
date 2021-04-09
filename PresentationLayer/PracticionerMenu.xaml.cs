ccb292d4c312d6be4c832cd904a8813509ac3830
Alemin <ale_200200@hotmail.com>
2021-04-09 12:05:11 -0500

    Correción de validación de precondiciones Solicitar proyecto

--- PresentationLayer/PracticionerMenu.xaml.cs
+++ PresentationLayer/PracticionerMenu.xaml.cs
@@ -3,6 +3,7 @@
 using DataPersistenceLayer;
 using DataPersistenceLayer.Entities;
 using DataPersistenceLayer.UnitsOfWork;
+using System.Collections.Generic;
 using System.Data.Entity.Core;
 using System.Windows;
 
@@ -77,6 +78,36 @@ namespace PresentationLayer
                 else
                 {
                     MessageBox.Show("No tiene un proyecto asignado. Contacte a su coordinador", "Consulta Fallida", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (EntityException)
+            {
+                MessageBox.Show("No hay conexión a la base de datos. Intente más tarde", "Consulta Fallida", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                unitOfWork.Dispose();
+            }
+        }
+
+        private void RequestProject(object sender, RoutedEventArgs routedEventArgs)
+        {
+            ProfessionalPracticesContext professionalPracticesContext = new ProfessionalPracticesContext();
+            UnitOfWork unitOfWork = new UnitOfWork(professionalPracticesContext);
+            try
+            {
+                bool practicionerHaveAProject = unitOfWork.Practicioners.PracticionerHasActiveProject(_practicionerEnrollment);
+                IList<Project> projecsAvailableForThosPracticioner = unitOfWork.Projects.GetProjectsAvailableToRequest(_practicionerEnrollment);
+                int requestMade = unitOfWork.RequestProjects.GetPracticionerRequest(_practicionerEnrollment);
+                if (practicionerHaveAProject || projecsAvailableForThosPracticioner.Count == 0 || requestMade == 3 )
+                {
+                    MessageBox.Show("Ya has solocitado un proyecto o no hay proyectos disponibles", "Consulta Fallida", MessageBoxButton.OK, MessageBoxImage.Error);      
+                }
+                else
+                {
+                    RequestProjects requestProject = new RequestProjects(_practicionerEnrollment);
+                    requestProject.Show();
+                    Close();
                 }
             }
             catch (EntityException)
