5c738e83b8985628769a43b0f1e2ad0b80aee1bc
Alemin <ale_200200@hotmail.com>
2021-04-12 23:08:35 -0500

    ModifyActivity

--- PresentationLayer/ManageReportsModifyActivity.xaml.cs
+++ PresentationLayer/ManageReportsModifyActivity.xaml.cs
@@ -1,16 +1,12 @@
-﻿using System;
+﻿using DataPersistenceLayer;
+using DataPersistenceLayer.Entities;
+using DataPersistenceLayer.UnitsOfWork;
+using FluentValidation.Results;
+using PresentationLayer.Validators;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PresentationLayer
 {
@@ -20,10 +16,31 @@ namespace PresentationLayer
     public partial class ManageReportsModifyActivity : Window
     {
         private string _staffNumber;
+        public Activity Activity = new Activity();
+        private readonly ProfessionalPracticesContext _professionalPracticesContext;
+        private readonly UnitOfWork _unitOfWork;
         public ManageReportsModifyActivity(string staffNumber, int idActivity)
         {
             _staffNumber = staffNumber;
+            _professionalPracticesContext = new ProfessionalPracticesContext();
+            _unitOfWork = new UnitOfWork(_professionalPracticesContext);
             InitializeComponent();
+            LoadActivity(idActivity);
+            this.DataContext = Activity;
+        }
+
+        private void LoadActivity(int idActivity)
+        {
+            try
+            {
+                Activity = _unitOfWork.Activities.Get(idActivity);
+                
+            }
+            catch (SqlException)
+            {
+                _unitOfWork.Dispose();
+                CatchDBException();
+            }
         }
         private void CancelButtonClicked(object sender, RoutedEventArgs routedEventArgs)
         {
@@ -32,6 +49,86 @@ namespace PresentationLayer
 
         private void ModifyButtonClicked(object sender, RoutedEventArgs routedEventArgs)
         {
+            CreateActivity();
+            if (ValidateData())
+            {
+                try
+                {
+                    _unitOfWork.Complete();
+                    MessageBox.Show("Actividad modificada exitosamente.");
+                    this.Close();
+                }
+                catch (SqlException)
+                {
+                    CatchDBException();
+                }
+                finally
+                {
+                    _unitOfWork.Dispose();
+                }
+            }
+        }
+
+        private void CatchDBException()
+        {
+            MessageBox.Show("No hay conexión con la base de datos. Intente más tarde.");
+            TeacherMenu teacherMenu = new TeacherMenu(_staffNumber);
+            teacherMenu.Show();
+            this.Close();
+        }
+
+        private bool ValidateData()
+        {
+            bool isValid = false;
+            ActivityValidator activityValidator = new ActivityValidator();
+            ValidationResult dataValidationResult = activityValidator.Validate(Activity);
+
+            IList<ValidationFailure> validationFailures = dataValidationResult.Errors;
+            UserFeedback userFeedback = new UserFeedback(FormGrid, validationFailures);
+            userFeedback.ShowFeedback();
+            foreach (ValidationFailure v in validationFailures)
+            {
+                Console.WriteLine(v);
+            }
+            if (dataValidationResult.IsValid)
+            {
+                isValid = true;
+            }
+            return isValid;
+        }
+
+        private void CreateActivity()
+        {
+            string date = DatePickerDate.SelectedDate.ToString();
+            string hour = TimePickerHour.SelectedTime.ToString();
+            Activity.FinishDate = Convert.ToDateTime(date + " " + hour);
+
+            int selectedType = int.Parse(((System.Windows.Controls.ComboBoxItem)ComboBoxType.SelectedItem).Tag.ToString());
+            if (selectedType == 0)
+            {
+                Activity.ActivityType = ActivityType.MonthlyReport;
+            }
+            else
+            {
+                Activity.ActivityType = ActivityType.PartialReport;
+            }
+
+            int selectedStatus = int.Parse(((System.Windows.Controls.ComboBoxItem)ComboBoxStatus.SelectedItem).Tag.ToString());
+            if (selectedStatus == 0)
+            {
+                Activity.ActivityStatus = ActivityStatus.CANCELLED;
+            }
+            else
+            {
+                if (selectedStatus == 1)
+                {
+                    Activity.ActivityStatus = ActivityStatus.ACTIVE;
+                }
+                else
+                {
+                    Activity.ActivityStatus = ActivityStatus.FINISHED;
+                }
+            }
 
         }
     }
---
+++ PresentationLayer/Validators/ActivityValidator.cs
@@ -0,0 +1,51 @@
+﻿using DataPersistenceLayer.Entities;
+using FluentValidation;
+using System;
+using System.Text.RegularExpressions;
+
+namespace PresentationLayer.Validators
+{
+    public class ActivityValidator : AbstractValidator<Activity>
+    {
+        public ActivityValidator()
+        {
+            RuleFor(activity => activity.Name).NotEmpty().WithState(user => "TextBoxName")
+                .MaximumLength(150).WithState(user => "TextBoxName");
+
+            RuleFor(activity => activity.Description).NotEmpty().WithState(user => "TextBoxDescription")
+                .MaximumLength(150).WithState(user => "TextBoxDescription");
+
+            RuleFor(activity => activity.ActivityType).IsInEnum();
+
+            RuleFor(activity => activity.ValueActivity).NotEmpty().Must(ValidValue).WithState(activity => "TextBoxValue");
+
+            RuleFor(activity => activity.ActivityStatus).IsInEnum();
+
+            RuleFor(activity => activity.StartDate).NotEmpty().Must(ValidStartDate);
+
+            RuleFor(activity => activity.FinishDate).NotEmpty().Must(ValidStartDate);
+        }
+
+        public bool ValidValue(double value)
+        {
+            Regex regularExpression = new Regex("^[0-9]{2}$");
+            string valueString = value.ToString();
+            bool hasValidFormat = regularExpression.IsMatch(valueString);
+            if (value > 100 || value < 1)
+            {
+                return false;
+            }
+            return hasValidFormat;
+        }
+
+        public bool ValidStartDate(DateTime? dateTime)
+        {
+            DateTime dateNow = DateTime.UtcNow;
+            if (dateTime < dateNow)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
